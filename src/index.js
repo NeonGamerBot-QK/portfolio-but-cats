0a1bf70a5c963db539bc23870ba1b299fd906ec6
Neon <neon@saahild.com>
2025-10-19 17:07:46 -0400

    feat: init but in pug now

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+app.set("view engine", "pug")
+app.set("views", __dirname + "/views")
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+    res.render('index', { name: 'Saahil' })
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`)
+})
