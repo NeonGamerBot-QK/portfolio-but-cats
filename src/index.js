7903c0e91ed0aa46e0cc484226610f769bbe207f
Neon <neon@saahild.com>
2025-10-19 23:06:04 -0400

    update: pug pug pug

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
+app.use(express.static(__dirname + "/public"));
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
